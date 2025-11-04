8b78f7d5d6482128326af4bb43a6ae1bdad13902
ianzyong <ianzyong@gmail.com>
2025-11-04 18:57:07 -0500

    config and alpha-delta ratio

--- 01-sleep_benchmark.py
+++ 01-sleep_benchmark.py
@@ -1,34 +1,12 @@
 # 01-sleep_benchmark.py
 
-### Parameters to edit ###
-
-# BIDS root
-bids_root = r"C:\Users\ianzy\Documents\Research\sleep-benchmark\Michigan_Epilepsy_Data\BIDS"
-
-# BIDS path information
-subject = "umich0020"
-session = "ieeg01"
-datatype = "ieeg"
-task = "all"
-run = "01"
-suffix = "ieeg"
-extension = ".lay"
-
-# list of staging methods to use, e.g. ['yasa', 'ad_scalp', 'ad_ieeg', 'ad_all']
-staging_methods = ['yasa','ad_scalp','ad_ieeg','ad_all']
-
-# window start for sleep staging (seconds from start of recording)
-window_start = 5*3600
-
-# window length for sleep staging (seconds from start of recording)
-window_length = 1*3600
-
-### End of parameters ###
-
+import os
+import configparser
 import matplotlib.pyplot as plt
 import numpy as np
 import time
 import mne
+import yaml
 from datetime import datetime
 from datetime import timedelta
 import yasa
@@ -101,9 +79,8 @@ def get_yasa_consensus_stages(raw):
     consensus_stage = determine_consensus_stage(predicted_c3, predicted_cz, predicted_c4)
     return consensus_stage
 
-def get_alphadelta_stages(raw, method_name):
+def get_alphadelta_stages(raw, picks, threshold_ratio):
     # calculate alpha/delta ratio on each channel
-    threshold_ratio = 0.25  # threshold ratio to separate sleep vs wake
     sfreq = raw.info['sfreq']
     epoch_length = 30  # seconds
     n_epochs = int(np.floor(raw.n_times / (sfreq * epoch_length)))
@@ -111,19 +88,7 @@ def get_alphadelta_stages(raw, method_name):
     for epoch in range(n_epochs):
         start_sample = int(epoch * epoch_length * sfreq)
         end_sample = int((epoch + 1) * epoch_length * sfreq)
-        if method_name == 'ad_scalp':
-            # pick all scalp EEG channels, ignore EKG
-            epoch_data = raw.get_data(picks=mne.pick_types(raw.info, eeg=True, exclude=['EKG1','EKG2']), start=start_sample, stop=end_sample)
-        elif method_name == 'ad_ieeg':
-            # pick all iEEG channels, ignore EKG
-            epoch_data = raw.get_data(picks=mne.pick_types(raw.info, seeg=True, exclude=['EKG1','EKG2']), start=start_sample, stop=end_sample)
-        elif method_name == 'ad_all':
-            # pick all scalp EEG and iEEG channels, ignore EKG
-            epoch_data = raw.get_data(picks=mne.pick_types(raw.info, eeg=True, seeg=True, exclude=['EKG1','EKG2']), start=start_sample, stop=end_sample)
-        else:
-            print(f"Method name {method_name} not recognized for alpha/delta staging. Defaulting to scalp EEG channels.")
-            # pick all scalp EEG channels, ignore EKG
-            epoch_data = raw.get_data(picks=mne.pick_types(raw.info, eeg=True, exclude=['EKG1','EKG2']), start=start_sample, stop=end_sample)
+        epoch_data = raw.get_data(picks=picks, start=start_sample, stop=end_sample)
         # compute power spectral density
         psd, freqs = mne.time_frequency.psd_array_welch(epoch_data, sfreq=sfreq, fmin=0.5, fmax=12, n_fft=8192, verbose=False)
         # compute alpha power (8-12 Hz)
@@ -173,137 +138,213 @@ def get_percent_agreement(run_events, predicted_stages, window_start, stage_dura
     percent_agreement = (match_count / len(predicted_stages)) * 100
     return percent_agreement
 
-# BIDS path
-bids_path = BIDSPath(
-    subject=subject,
-    session=session,
-    datatype=datatype,
-    task=task,
-    run=run,
-    suffix=suffix,
-    extension=extension,
-    root=bids_root
-)
+# config = configparser.ConfigParser()
+# config.read(os.path.join(os.path.dirname(__file__), "01-config.ini"))
+# print(config.sections())
+# bids_root = config['PARAMS']['bids_root']
+# bids_path_list = config['PARAMS']['bids_path_list']
 
-print(repr(bids_path))
+with open(os.path.join(os.path.dirname(__file__), "01-config.yaml"), 'r') as file:
+    config = yaml.safe_load(file)
 
-raw = mne.io.read_raw_persyst(bids_path)
+bids_root = config['PARAMS']['bids_root']
+bids_path_list = config['PARAMS']['bids_path_list']
 
-for annot in raw.annotations:
-    if annot["description"] in ['W', 'N1', 'N2', 'N3', 'REM']:  
-        #print(f"{annot['onset']}, {annot['duration']}, {annot['description']}")
-        continue
+total_processing_time = 0
 
-# read events.tsv in the same folder
-events_tsv_path = bids_path.copy().update(suffix="events", extension=".tsv", task=None, run=None)
+for param_dict in bids_path_list:
+    # start timer
+    start_time = time.time()
 
-# get all events where event_category = "vigilance"
-events_data = np.loadtxt(events_tsv_path.fpath, dtype=str, delimiter="\t", skiprows=1)
-vigilance_events = events_data[events_data[:,2] == "vigilance"]
+    subject = param_dict['subject']
+    session = param_dict['session']
+    datatype = param_dict['datatype']
+    task = param_dict['task']
+    run = param_dict['run']
+    suffix = param_dict['suffix']
+    extension = param_dict['extension']
+    staging_methods = param_dict['staging_methods']
+    stage_full_recording = param_dict['stage_full_recording']
+    save_figures = param_dict['save_figures']
 
-# print test date of this run
-test_date = raw.info['meas_date'].strftime('%Y-%m-%d')
-test_time = raw.info['meas_date'].strftime('%H:%M:%S')
-test_duration = raw.n_times / raw.info['sfreq']
-earliest_date = get_earliest_date(events_tsv_path)
+    # BIDS path
+    bids_path = BIDSPath(
+        subject=subject,
+        session=session,
+        datatype=datatype,
+        task=task,
+        run=run,
+        suffix=suffix,
+        extension=extension,
+        root=bids_root
+    )
 
-def date_to_seconds(date_str, earliest_date_str = earliest_date):
-    # converts a date string in the format YYYY-MM-DD to seconds since the earliest date
-    date = datetime.strptime(date_str, "%Y-%m-%d")
-    earliest_date = datetime.strptime(earliest_date_str, "%Y-%m-%d")
-    delta = date - earliest_date
-    return delta.days * 86400
+    print(repr(bids_path))
 
-# if subject = "umich0020", test year should be 2010
-if subject == "umich0020":
-    test_date = test_date.replace("2013", "2010")
-print(f"Test date of this run: {test_date}. Test time: {test_time}. Test duration (s): {test_duration}")
-print(f"Earliest date in events.tsv: {earliest_date}")
+    raw = mne.io.read_raw_persyst(bids_path)
 
-# get events for this run
-run_events = [event for event in vigilance_events if ((date_to_seconds(event[0].split("T")[0]) + time_to_seconds(event[0].split("T")[1]) > (date_to_seconds(test_date) + time_to_seconds(test_time))) and (date_to_seconds(event[0].split("T")[0]) + time_to_seconds(event[0].split("T")[1]) <= (date_to_seconds(test_date) + time_to_seconds(test_time) + test_duration)))]
-# merge to numpy array
-run_events = np.array(run_events)
+    for annot in raw.annotations:
+        if annot["description"] in ['W', 'N1', 'N2', 'N3', 'REM']:  
+            #print(f"{annot['onset']}, {annot['duration']}, {annot['description']}")
+            continue
 
-# crop raw data to window_length seconds from window_start
-raw.crop(window_start, window_start + window_length)
+    # read channels.tsv for this run to determine channel types
+    channels_tsv_path = bids_path.copy().update(suffix="channels", extension=".tsv")
+    channels_data = np.loadtxt(channels_tsv_path.fpath, dtype=str, delimiter="\t", skiprows=1)
+    scalp_channel_names = channels_data[channels_data[:,1] == "EEG"][:,0].tolist()
+    ieeg_channel_names = channels_data[channels_data[:,1] == "SEEG"][:,0].tolist()
+    # convert electrode names to uppercase
+    scalp_channel_names = [name.upper() for name in scalp_channel_names]
+    ieeg_channel_names = [name.upper() for name in ieeg_channel_names]
 
-# extract time from beginning and corresponding state from run_events
-# convert second column to cumulative sum
-event_times = np.cumsum(run_events[:,1].astype(float))
-event_times = np.insert(event_times, 0, 0) # insert 0 as first element
-event_times = event_times[:-1] # remove last element
-event_states = run_events[:,3]
+    # read events.tsv in the same folder
+    events_tsv_path = bids_path.copy().update(suffix="events", extension=".tsv", task=None, run=None)
 
-# map vigilance states to numerical values for plotting
-y_dict = {'N3': 0, 'N2': 1, 'N1': 2, 'REM': 3, 'wake': 4}
-event_y = [y_dict[state] for state in event_states]
-# convert to hours
-event_x = [time/3600 for time in event_times]
+    # get all events where event_category = "vigilance"
+    events_data = np.loadtxt(events_tsv_path.fpath, dtype=str, delimiter="\t", skiprows=1)
+    vigilance_events = events_data[events_data[:,2] == "vigilance"]
 
-# plot vigilance states over time
-print("Plotting hypnogram of entire run...")
-plt.figure(figsize=(10, 4))
-plt.step(event_x, event_y, where='post')
-plt.xlabel(f'Time (hrs) (t=0 is {test_time})')
-plt.ylabel('Vigilance State')
-plt.title(f'Hypnogram for {subject}, session {session}, task {task}, run {run}')
-plt.ylim(-0.5, 4.5)
-plt.yticks([0, 1, 2, 3, 4], ['N3', 'N2', 'N1', 'REM', 'W'])
-plt.grid()
-plt.show()
+    # print test date of this run
+    test_date = raw.info['meas_date'].strftime('%Y-%m-%d')
+    test_time = raw.info['meas_date'].strftime('%H:%M:%S')
+    test_duration = raw.n_times / raw.info['sfreq']
+    earliest_date = get_earliest_date(events_tsv_path)
 
-print(f"Running automated sleep staging methods from {window_start/3600} to {(window_start+window_length)/3600} hours...")
+    def date_to_seconds(date_str, earliest_date_str = earliest_date):
+        # converts a date string in the format YYYY-MM-DD to seconds since the earliest date
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        earliest_date = datetime.strptime(earliest_date_str, "%Y-%m-%d")
+        delta = date - earliest_date
+        return delta.days * 86400
 
-# start timer
-start_time = time.time()
+    # if subject = "umich0020", test year should be 2010
+    if subject == "umich0020":
+        test_date = test_date.replace("2013", "2010")
+    print(f"Test date of this run: {test_date}. Test time: {test_time}. Test duration (s): {test_duration}")
+    print(f"Earliest date in events.tsv: {earliest_date}")
 
-for method in staging_methods:
-    print(f"Using staging method: {method}")
-    if method == 'yasa':
-        predicted_stages = get_yasa_consensus_stages(raw.copy())
-    elif method[0:3] == 'ad_':
-        predicted_stages, avg_ad_ratios = get_alphadelta_stages(raw.copy(), method)
-        # plot average alpha/delta ratios over time
-        plt.figure(figsize=(10, 4))
-        ad_x = np.arange(window_start, window_start + len(avg_ad_ratios)*30, 30) / 3600  # assuming 30-second epochs
-        plt.plot(ad_x, avg_ad_ratios, marker='o')
-        plt.xlabel(f'Time (hrs) (t=0 is {test_time})')
-        plt.ylabel('Average Alpha/Delta Ratio')
-        plt.title(f'Average Alpha/Delta Ratios for {subject}, session {session}, task {task}, run {run}')
-        plt.xlim(window_start/3600, (window_start+window_length)/3600)
-        plt.grid()
-        plt.show()
+    # get events for this run
+    run_events = [event for event in vigilance_events if ((date_to_seconds(event[0].split("T")[0]) + time_to_seconds(event[0].split("T")[1]) > (date_to_seconds(test_date) + time_to_seconds(test_time))) and (date_to_seconds(event[0].split("T")[0]) + time_to_seconds(event[0].split("T")[1]) <= (date_to_seconds(test_date) + time_to_seconds(test_time) + test_duration)))]
+    # merge to numpy array
+    run_events = np.array(run_events)
+
+    if stage_full_recording:
+        window_start = 0
+        window_stop = test_duration
+        print("Staging full recording...")
     else:
-        print(f"Staging method {method} not recognized. Skipping...")
-        continue
+        window_start = param_dict['window_start']
+        window_stop = param_dict['window_stop']
 
-    print(f"Predicted stages: {predicted_stages}")
-    # calculate overlap coefficient between consensus_stages and manual stages
-    print("Calculating percent agreement between consensus stages and manual stages...")
-    percent_agreement = get_percent_agreement(run_events, predicted_stages, window_start, 30)
-    print(f"Percent agreement between consensus stages and manual stages: {percent_agreement:.2f}%")
+    # crop raw data to window_length seconds from window_start
+    raw.crop(window_start, window_stop)
 
-    # x values for consensus stages
-    stage_x = np.arange(window_start, window_start + len(predicted_stages)*30, 30) / 3600  # assuming 30-second epochs
-    y_dict_prediction = {'N3': 0, 'N2': 1, 'N1': 2, 'R': 3, 'W': 4, 'sleep': np.nan, np.nan: np.nan}
-    stage_y = [y_dict_prediction[stage] for stage in predicted_stages]
+    # extract time from beginning and corresponding state from run_events
+    # convert second column to cumulative sum
+    event_times = np.cumsum(run_events[:,1].astype(float))
+    event_times = np.insert(event_times, 0, 0) # insert 0 as first element
+    event_times = event_times[:-1] # remove last element
+    event_states = run_events[:,3]
 
-    print("Plotting hypnogram for cropped data...")
+    # map vigilance states to numerical values for plotting
+    y_dict = {'N3': 0, 'N2': 1, 'N1': 2, 'REM': 3, 'wake': 4}
+    event_y = [y_dict[state] for state in event_states]
+    # convert to hours
+    event_x = [time/3600 for time in event_times]
+
+    # plot vigilance states over time
+    print("Plotting hypnogram of entire run...")
     plt.figure(figsize=(10, 4))
-    plt.step(event_x, event_y, where='post', label='Manual Staging', color='tab:blue', alpha=0.5)
-    plt.xlim(window_start/3600, (window_start+window_length)/3600)
+    plt.step(event_x, event_y, where='post')
     plt.xlabel(f'Time (hrs) (t=0 is {test_time})')
     plt.ylabel('Vigilance State')
-    plt.title(f'Cropped hypnogram for {subject}, session {session}, task {task}, run {run}')
+    plt.title(f'Hypnogram for {subject}, session {session}, task {task}, run {run}')
     plt.ylim(-0.5, 4.5)
     plt.yticks([0, 1, 2, 3, 4], ['N3', 'N2', 'N1', 'REM', 'W'])
-    plt.step(stage_x, stage_y, where='post', label=f'Staging: {method}, {percent_agreement:.2f}% agreement', color='tab:orange', alpha=0.5)
-    plt.legend()
+    plt.grid()
 
-# end timer
-end_time = time.time()
-print(f"Total processing time = {timedelta(seconds=end_time-start_time)}")
+    if save_figures:
+        # save to figures folder
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), "figures", subject)):
+            os.makedirs(os.path.join(os.path.dirname(__file__), "figures", subject), exist_ok=True)
+        
+        plt.savefig(os.path.join(os.path.dirname(__file__), "figures", subject, f'{subject}_{session}_{task}_{run}_full_hypnogram.png'))
 
-plt.grid()
-plt.show()
+    plt.show()
+
+    print(f"Running automated sleep staging methods from {window_start/3600} to {(window_stop)/3600} hours...")
+
+    for method in staging_methods:
+        print(f"Using staging method: {method}")
+        if method == 'yasa':
+            predicted_stages = get_yasa_consensus_stages(raw.copy())
+        elif method[0:3] == 'ad_':
+            if method == 'ad_scalp':
+                picks = scalp_channel_names
+            elif method == 'ad_ieeg':
+                picks = ieeg_channel_names
+            elif method == 'ad_all':
+                picks = scalp_channel_names + ieeg_channel_names
+            else:
+                print(f"Method name {method} not recognized for alpha/delta staging. Defaulting to scalp EEG channels.")
+                picks = scalp_channel_names
+            threshold_ratio = 0.25  # threshold ratio to separate sleep vs wake    
+            predicted_stages, avg_ad_ratios = get_alphadelta_stages(raw.copy(), picks, threshold_ratio)
+            # plot average alpha/delta ratios over time
+            plt.figure(figsize=(10, 4))
+            ad_x = np.arange(window_start, window_start + len(avg_ad_ratios)*30, 30) / 3600  # assuming 30-second epochs
+            plt.plot(ad_x, avg_ad_ratios, marker='o')
+            plt.xlabel(f'Time (hrs) (t=0 is {test_time})')
+            plt.ylabel('Average Alpha/Delta Ratio')
+            plt.title(f'Average alpha/delta ratios for {subject}, session {session}, task {task}, run {run} ({method})')
+            plt.xlim(window_start/3600, (window_stop)/3600)
+            plt.axhline(y=threshold_ratio, color='r', linestyle='--')
+            plt.grid()
+            
+            if save_figures:
+                # save to figures folder
+                if not os.path.exists(os.path.join(os.path.dirname(__file__), "figures", subject)):
+                    os.makedirs(os.path.join(os.path.dirname(__file__), "figures", subject), exist_ok=True)
+                
+                plt.savefig(os.path.join(os.path.dirname(__file__), "figures", subject, f'{subject}_{session}_{task}_{run}_{window_start}_{window_stop}_avg_ad_ratios_{method}.png'))
+            plt.show()
+        else:
+            print(f"Staging method {method} not recognized. Skipping...")
+            continue
+
+        print(f"Predicted stages: {predicted_stages}")
+        # calculate overlap coefficient between predicted_stages and manual stages
+        percent_agreement = get_percent_agreement(run_events, predicted_stages, window_start, 30)
+        print(f"Percent agreement between predicted stages and manual stages: {percent_agreement:.2f}%")
+
+        # x values for predicted stages
+        stage_x = np.arange(window_start, window_start + len(predicted_stages)*30, 30) / 3600  # assuming 30-second epochs
+        y_dict_prediction = {'N3': 0, 'N2': 1, 'N1': 2, 'R': 3, 'W': 4, 'sleep': np.nan, np.nan: np.nan}
+        stage_y = [y_dict_prediction[stage] for stage in predicted_stages]
+
+        print("Plotting hypnogram for cropped data...")
+        plt.figure(figsize=(10, 4))
+        plt.step(event_x, event_y, where='post', label='Manual Staging', color='tab:blue', alpha=0.5)
+        plt.xlim(window_start/3600, (window_stop)/3600)
+        plt.xlabel(f'Time (hrs) (t=0 is {test_time})')
+        plt.ylabel('Vigilance State')
+        plt.title(f'Cropped hypnogram for {subject}, session {session}, task {task}, run {run}')
+        plt.ylim(-0.5, 4.5)
+        plt.yticks([0, 1, 2, 3, 4], ['N3', 'N2', 'N1', 'REM', 'W'])
+        plt.step(stage_x, stage_y, where='post', label=f'Staging: {method}, {percent_agreement:.2f}% agreement', color='tab:orange', alpha=0.5)
+        plt.legend()
+        plt.grid()
+        
+        if save_figures:
+            # save to figures folder
+            if not os.path.exists(os.path.join(os.path.dirname(__file__), "figures", subject)):
+                os.makedirs(os.path.join(os.path.dirname(__file__), "figures", subject), exist_ok=True)
+            
+            plt.savefig(os.path.join(os.path.dirname(__file__), "figures", subject, f'{subject}_{session}_{task}_{run}_{window_start}_{window_stop}_hypnogram_{method}.png'))
+        plt.show()
+    # end timer
+    end_time = time.time()
+    total_processing_time += end_time - start_time
+    print(f"Processing time = {timedelta(seconds=end_time-start_time)}")
+
+print(f"Total processing time = {timedelta(seconds=total_processing_time)}")
